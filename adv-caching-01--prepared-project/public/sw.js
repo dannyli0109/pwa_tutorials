bcecc98b609eceac41c19752daa3c7ef8cd81203
Danny Li <Danny.Li@clemenger.com.au>
2018-03-22 15:20:10 +1100

    adv caching assignment

--- adv-caching-01--prepared-project/public/sw.js
+++ adv-caching-01--prepared-project/public/sw.js
@@ -1,6 +1,22 @@
 
 var CACHE_STATIC_NAME = 'static-v12';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+]
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
@@ -8,22 +24,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_STATIC_NAME)
       .then(function(cache) {
         console.log('[Service Worker] Precaching App Shell');
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/offline.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/promise.js',
-          '/src/js/fetch.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
+        cache.addAll(STATIC_FILES);
       })
   )
 });
@@ -45,16 +46,50 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CACHE_DYNAMIC_NAME)
-      .then(cache => {
-        return fetch(event.request)
-          .then(res => {
-            cache.put(event.request, res.clone())
-            return res
-          })
-      })
-  );
+  let url = 'https://httpbin.org/get'
+
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_DYNAMIC_NAME)
+        .then(cache => {
+          return fetch(event.request)
+            .then(res => {
+              cache.put(event.request, res.clone())
+              return res
+            })
+        })
+    );
+  } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b')).test(event.request.url)) {
+      event.respondWith(
+        caches.match(event.request)
+      )
+  } else {
+      event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          } else {
+            return fetch(event.request)
+              .then(function(res) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(function(cache) {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                  })
+              })
+              .catch(function(err) {
+                return caches.open(CACHE_STATIC_NAME)
+                  .then(cache => {
+                    if (event.request.url.indexOf('/help')) {
+                      return cache.match('/offline.html')                      
+                    } 
+                  })
+              });
+          }
+        })
+      )
+  }
 });
 
 // self.addEventListener('fetch', function(event) {
---
+++ problem--adv-caching-assignment/public/sw.js
@@ -0,0 +1,128 @@
+
+var CACHE_STATIC_NAME = 'static-v8';
+var CACHE_DYNAMIC_NAME = 'dynamic-v7';
+var STATIC_FILE_PATH = [
+  '/',
+  '/index.html',
+  '/src/css/app.css',
+  '/src/css/main.css',
+  '/src/js/main.js',
+  '/src/js/material.min.js',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function(cache) {
+        cache.addAll(STATIC_FILE_PATH);
+      })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME) {
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+  
+});
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         } else {
+//           return fetch(event.request)
+//             .then(function(res) {
+//               return caches.open(CACHE_DYNAMIC_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request.url, res.clone());
+//                   return res;
+//                 });
+//             })
+//             .catch(function(err) {
+
+//             });
+//         }
+//       })
+//   );
+// });
+
+// // network-only
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(fetch(event.request));
+// });
+
+// // cache-only
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(res => res)
+//   );
+// });
+
+// // network, and cache fallback
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request)
+//       .then(res => {
+//         return caches.open(CACHE_DYNAMIC_NAME)
+//           .then(cache => {
+//             cache.put(event.request.url, res.clone())
+//             return res
+//           })
+//       })
+//       .catch(err => {
+//         return caches.match(event.request)
+//       })
+//   );
+// });
+
+let url = 'https://httpbin.org/ip'
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_DYNAMIC_NAME)
+        .then(cache => {
+          return fetch(event.request)
+            .then(res => {
+              cache.put(event.request.url, res.clone())
+              return res
+            })
+        })
+    );
+  } 
+  else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(res => {
+          if (res) {
+            return res
+          } else {
+            return fetch(event.request)
+              .then(response => {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(cache => {
+                    cache.put(event.request.url, response.clone())
+                    return response
+                  })
+              })
+              .catch(err => {
+                console.log(err)
+              })
+          }
+        })
+    )
+  } 
+});
